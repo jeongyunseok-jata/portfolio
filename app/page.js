24eefda655aeff5dffca0ccd27d445cc64a8dcd0
정윤석 <jeong-yunseok@jeong-yunseog-ui-MacBookAir.local>
2023-05-02 01:30:24 +0900

    edit

--- app/page.js
+++ app/page.js
@@ -1,66 +1,38 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import Lottie from 'react-lottie-player';
+import arrow from '../public/arrow.svg';
+import lottieJson from '../public/animations/main.json';
 
 export default function Home() {
   return (
-    <div>
-      <div className='mt-24 ml-9 sm:ml-4 text-4xl font-medium text-white'>DevOn Projects</div>
-      <div className='flex flex-wrap'>
-        <ProjCard />
-        <ProjCard />
-        <ProjCard />
-      </div>
-      <div className='mt-24 ml-9 mb-9 sm:ml-4 text-4xl font-medium text-white'>Web Develop Process</div>
-      <div className='md:flex md:justify-between md:items-center pl-9 pr-9 sm:pl-4 sm:pr-4'>
-        <div className='flex items-center'>
-          <div className='flex items-center justify-center bg-blue-900 rounded-full h-16 w-16 xl:h-16 xl:w-16 md:w-11 md:h-11'>
-            <span className='text-white font-bold text-2xl xl:text-2xl md:text-xl'>1</span>
-          </div>
-          <span className='ml-3 xl:text-2xl md:text-xl text-2xl font-bold text-white'>Consulting</span>
+    <>
+      <section className='min-h-screen flex flex-col justify-center items-center'>
+        <div className='max-w-md'>
+          <Lottie
+            loop
+            animationData={lottieJson}
+            play
+          />
         </div>
-        <div className='flex items-center'>
-          <hr className='flex-1 border-t-2 border-dashed border-gray-300 w-14 xl:w-36 md:w-25 mt-6 md:mt-0'/>
+        <div className='text-center mt-12'>
+          <h1 className='text-4xl font-medium text-gray-600 mb-3'>
+            Wellcome DevOn!
+          </h1>
+          <p className='text-lg text-gray-500'>
+            This site introduces Jung Yun-seok's portfolio&services.
+          </p>
+          <Image
+            src={arrow}
+            className='w-9 h-9 absolute left-1/2 tr-arrow bottom-4'
+          />
         </div>
-        <div className='flex items-center'>
-          <div className='flex items-center justify-center bg-blue-900 rounded-full h-16 w-16 xl:h-16 xl:w-16 md:w-11 md:h-11 mt-6 md:mt-0'>
-            <span className='text-white font-bold xl:text-2xl md:text-xl text-2xl'>2</span>
-          </div>
-          <span className='ml-3 font-bold xl:text-2xl md:text-xl text-2xl mt-6 md:mt-0 text-white'>Develop</span>
-        </div>
-        <div className='flex items-center'>
-          <hr className='flex-1 border-t-2 border-dashed border-gray-300 w-14 xl:w-36 md:w-25 mt-6 md:mt-0'/>
-        </div>
-        <div className='flex items-center'>
-          <div className='flex items-center justify-center bg-blue-900 rounded-full h-16 w-16 xl:h-16 xl:w-16 md:w-11 md:h-11 mt-6 md:mt-0'>
-            <span className='text-white font-bold xl:text-2xl md:text-xl text-2xl'>3</span>
-          </div>
-          <span className='ml-3 font-bold xl:text-2xl md:text-xl text-2xl mt-6 md:mt-0 text-white'>Edit Details</span>
-        </div>
-        <div className='flex items-center'>
-          <hr className='flex-1 border-t-2 border-dashed border-gray-300 w-14 xl:w-36 md:w-25 mt-6 md:mt-0'/>
-        </div>
-        <div className='flex items-center'>
-          <div className='flex items-center justify-center bg-blue-900 rounded-full h-16 w-16 xl:h-16 xl:w-16 md:w-11 md:h-11 mt-6 md:mt-0'>
-            <span className='text-white font-bold xl:text-2xl md:text-xl text-2xl'>4</span>
-          </div>
-          <span className='ml-3 font-bold xl:text-2xl md:text-xl text-2xl mt-6 md:mt-0 text-white'>Complete</span>
-        </div>
-      </div>
-      
-    </div>
-  )
-}
+      </section>
 
-function ProjCard(){
-  return (
-    <Link href='https://frh.kr' className='p-4 sm:p-4 mx-auto w-11/12 sm:w-1/2 lg:w-1/3 h-96' id='Projcard'>
-      <div className='h-full rounded-lg overflow-hidden'>
-        <div className='h-full pf-1'>
-          <div className='pf-ct'>
-            <h1 className='text-2xl text-center font-semibold mb-2 text-white'>Frist Hosting</h1>
-          </div>
-        </div>
-      </div>
-    </Link>
+
+
+    </>
   )
 }
